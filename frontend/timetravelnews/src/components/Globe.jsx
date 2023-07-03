531822be4f67e7840a8893e30d078b7c6662cd20
Eti-ini Effiong-Robert <etinieffiongrobert@gmail.com>
2023-07-03 22:32:05 +0100

    Replaced map with placeholder

--- frontend/timetravelnews/src/components/Globe.jsx
+++ frontend/timetravelnews/src/components/Globe.jsx
@@ -1,6 +1,7 @@
 import { useRef, useState, useEffect } from "react"
 import { API_TOKENS } from '../data/API_TOKENS'
 import mapboxgl from 'mapbox-gl'
+import earthImage from '../assets/images/earth_image.png'
 
 mapboxgl.accessToken = API_TOKENS.mapbox;
 
@@ -11,21 +12,25 @@ const Globe = (props) => {
     const [lat, setLat] = useState(42.35);
     const [zoom, setZoom] = useState(1.5);
 
-    useEffect(() => {
-        if (map.current) return; // initialize map only once
+    // useEffect(() => {
+    //     if (map.current) return; // initialize map only once
 
-        map.current = new mapboxgl.Map({
-            container: mapContainer.current,
-            projection: 'globe',
-            style: 'mapbox://styles/mapbox/streets-v12',
-            center: [lng, lat],
-            zoom: zoom
-        });
-    });
+    //     map.current = new mapboxgl.Map({
+    //         container: mapContainer.current,
+    //         projection: 'globe',
+    //         style: 'mapbox://styles/mapbox/streets-v12',
+    //         center: [lng, lat],
+    //         zoom: zoom
+    //     });
+    // });
 
     return (
         <>
-            <div ref={mapContainer} className="map-container" style={{ width: "1000px", height: "600px" }} />
+            <h1>Hello</h1>
+            <p>{earthImage}</p>
+            <img src={earthImage}></img>
+            {/* This is the real globe code below. The image is just a placeholder */}
+            {/* <div ref={mapContainer} className="map-container" style={{ width: "1000px", height: "600px" }} /> */}
         </>
     )
 }
